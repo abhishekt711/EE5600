064915d7bff6f42574705df76abd65730f871d45
abhishekt711 <36338604+abhishekt711@users.noreply.github.com>
2020-09-21 12:46:53 +0530

    Update assignment_2.py

--- Assignment_2/code/assignment_2.py
+++ Assignment_2/code/assignment_2.py
@@ -10,15 +10,9 @@ event_no_head=120
 prob2=event_two_head/sample_size
 prob1=event_one_head/sample_size
 prob0=event_no_head/sample_size
-
 data_binom = binom.rvs(n=2,p=0.5,size=500)
-
-
-
-
 #Theory vs simulation
 print("Probability of Two Heads =",prob2,"\n","Probability of One Head =",prob1,"\n","Probability of No Head =", prob0)
-
 sns.set(color_codes=True)
 # settings for seaborn plot sizes
 sns.set(rc={'figure.figsize':(4.5,3)})
